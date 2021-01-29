8b2345c30efc99e6120807f064f06fdd5f1944d4
Ram Hiriyappa <rhiriyappa@yahoo.com>
2021-01-28 21:51:44 -0800

    index.js changes

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ client.connect(config.DB, { useNewUrlParser: true }, (err, db) => {
 });
 
 app.get('/', (req, res) => {
-    res.json("I love love docker !");
+    res.json("I love love... docker !");
 });
 
 app.listen(PORT, () => {
