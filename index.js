ae605bb34fe3f23378187298fb6796b1ccc511db
Ram Hiriyappa <rhiriyappa@mdsol.com>
2021-01-28 21:28:18 -0800

    commit to master

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ client.connect(config.DB, { useNewUrlParser: true }, (err, db) => {
 });
 
 app.get('/', (req, res) => {
-    res.json("I learned docker !");
+    res.json("I love docker !");
 });
 
 app.listen(PORT, () => {
