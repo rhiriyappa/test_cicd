d8349ccce78e1e49296deeef33c912e7e2ce79c5
Ram Hiriyappa <rhiriyappa@yahoo.com>
2021-01-28 21:57:39 -0800

    initial commit

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ client.connect(config.DB, { useNewUrlParser: true }, (err, db) => {
 });
 
 app.get('/', (req, res) => {
-    res.json("I love love docker !");
+    res.json("I love loved docker !");
 });
 
 app.listen(PORT, () => {
